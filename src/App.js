2313f8544c88f584867946d8d31a62e741b66bb2
Taha <mateen.naved456@gmail.com>
2023-08-04 20:52:57 +0500

    Slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navigation from './components/navigation/navigation.jsx';
 import IntroSearch from './components/introsearch/introsearch.jsx';
+import InfiniteSlider from './components/infiniteslider/infiniteslider';
 function App() {
   return (
    <>
    <Navigation/>
    <IntroSearch/>
+   <InfiniteSlider/>
    </>
   );
 }
---
+++ src/components/infiniteslider/infiniteslider.jsx
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react'
+import './infiniteslider.css'
+
+const InfiniteSlider = () => {
+  return (
+    <Fragment>
+<div className="slider">
+    <div className="slide-track">
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/personal-training.jpg!d=v1NTZ1" alt="" /><p>Personal Training</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/counselling.jpg!d=v1NTZ1" alt="" /><p>Counselling</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/house-cleaning.jpg!d=v1NTZ1" alt="" /><p>House Cleaning</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/accounting.jpg!d=v1NTZ1" alt="" /><p>Accounting</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/web-design.jpg!d=v1NTZ1" alt="" /><p>Web Design</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/gardening.jpg!d=v1NTZ1" alt="" /><p>Gardening</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/accounting.jpg!d=v1NTZ1" alt="" /><p>Accounting</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/counselling.jpg!d=v1NTZ1" alt="" /></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/personal-training.jpg!d=v1NTZ1" alt="" /><p>Personal Training</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/counselling.jpg!d=v1NTZ1" alt="" /><p>Counselling</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/house-cleaning.jpg!d=v1NTZ1" alt="" /><p>House Cleaning</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/accounting.jpg!d=v1NTZ1" alt="" /><p>Accounting</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/web-design.jpg!d=v1NTZ1" alt="" /><p>Web Design</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/gardening.jpg!d=v1NTZ1" alt="" /><p>Gardening</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/accounting.jpg!d=v1NTZ1" alt="" /><p>Accounting</p></div>
+        <div className="slide"><img src="https://d18jakcjgoan9.cloudfront.net/s/img/home/counselling.jpg!d=v1NTZ1" alt="" /></div>
+    </div>
+</div>
+<div class="media">
+	<div class="media-track">
+		<div class="med">
+			<img src="https://d18jakcjgoan9.cloudfront.net/s/img/frontend-v2/directory/press-logos/bbc.png!d=9gOhO"  alt="" />
+		</div>
+		<div class="med">
+			<img src="https://d18jakcjgoan9.cloudfront.net/s/img/frontend-v2/directory/press-logos/daily-mail.png!d=D6ZU1"  alt="" />
+		</div>
+		<div class="med">
+			<img src="https://d18jakcjgoan9.cloudfront.net/s/img/frontend-v2/directory/press-logos/the-guardian.png!d=yKyi0" alt="" />
+		</div>
+		<div class="med">
+			<img src="https://d18jakcjgoan9.cloudfront.net/s/img/frontend-v2/directory/press-logos/cosmopolitan.png!d=bebTX"  alt="" />
+		</div>
+	</div>
+</div>
+    </Fragment>
+  )
+}
+
+export default InfiniteSlider
