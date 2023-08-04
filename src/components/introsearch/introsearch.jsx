2135c126d44cab16c58d68198ffbdfdd423f65ef
Taha <mateen.naved456@gmail.com>
2023-08-04 12:00:19 +0500

    Intro Search

--- src/components/introsearch/introsearch.jsx
+++ src/components/introsearch/introsearch.jsx
@@ -1,27 +1,33 @@
-import React, { Fragment } from 'react'
-import './introsearch.css'
+import React, { Fragment } from "react";
+import "./introsearch.css";
 const IntroSearch = () => {
   return (
-   <Fragment>
-    <section className="frontbody">
-    <div className="text">
-        <h1>
+    <Fragment>
+      <div className="frontbody">
+        <div className="text">
+          <h1>
             Find the perfect<br></br> professional for you
-        </h1>
-        <h3>Get free quotes within minutes</h3>
-    </div>
-            <div className="boxes">
-                <input type="text" placeholder='What services are you looking for? ' />
-                <div className="locationicon"><i class="fa-solid fa-location-dot"></i></div>
-                <input type="text" className='Postcode' placeholder='Postcode' />
-                <button className="search">Search</button>
-            </div>
-            <div className="downtext">
-                <p>Popular: House Cleaning, Web Design, Personal Trainers</p>
-            </div>
-            </section>
+          </h1>
+          <h3>Get free quotes within minutes</h3>
+        </div>
+        <div className="boxes">
+          <input
+            type="text"
+            className="servicesinput"
+            placeholder="What services are you looking for? "
+          />
+          <div className="locationicon">
+            <i class="fa-solid fa-location-dot"></i>
+          </div>
+          <input type="text" className="Postcode" placeholder="Postcode" />
+          <button className="search">Search</button>
+        </div>
+        <div className="downtext">
+          <p>Popular: House Cleaning, Web Design, Personal Trainers</p>
+        </div>
+      </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default IntroSearch
+export default IntroSearch;
