2d75d802b139f0dc649e8c11a9cb7ccdd600e0df
Taha <mateen.naved456@gmail.com>
2023-09-06 12:20:07 +0500

    Task

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import Navigation from './components/navigation/navigation.jsx';
-import IntroSearch from './components/introsearch/introsearch.jsx';
-import InfiniteSlider from './components/infiniteslider/infiniteslider';
-import Discover from './components/discover/discover';
+import Tax from './components/tax/tax';
 
 function App() {
   return (
    <>
-   <Navigation/>
-   <IntroSearch/>
-   <InfiniteSlider/>
-   <Discover/>
-
+ <Tax/>
    </>
   );
 }
